669f24ab40022b8e34b0fca8c9e6eb3edcbcd608
James Bennetts <jamesbennetts@gmail.com>
2018-09-27 23:15:52 +0100

    Conditionals
    Completed conditionals module and two case studies - Magic Eight Ball and Race Day.

--- 2. Conditionals/Conditionals.js
+++ 2. Conditionals/Conditionals.js
@@ -240,8 +240,77 @@ if (season === 'spring') {
 
 
 
+// The Switch Keyword 
+
+// Else if version
+let groceryItem = 'papaya';
+
+if (groceryItem === 'tomato') {
+  console.log('Tomatoes are $0.49');
+} else if (groceryItem === 'papaya'){
+  console.log('Papayas are $1.29');
+} else {
+  console.log('Invalid item');
+}
+
+// Switch version
+let groceryItem = 'papaya';
+
+switch (groceryItem) {
+  case 'tomato':
+    console.log('Tomatoes are $0.49');
+    break;
+  case 'lime':
+    console.log('Limes are $1.49');
+    break;
+  case 'papaya':
+    console.log('Papayas are $1.29');
+ break;
+  default:
+    console.log('Invalid item');
+    break;
+}
+// Prints 'Papayas are $1.29'
 
 
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
+
+
+
+// REVIEW
+
+An if statement checks a condition and will execute a task if that condition evaluates to true.
+
+if...else statements make binary decisions and execute different code blocks based on a provided condition.
+
+We can add more conditions using else if statements.
+
+Comparison operators, including <, >, <=, >=, ===, and !== can compare two values.
+
+The logical and operator, &&, or "and", checks if both provided expressions are truthy.
+
+The logical operator ||, or "or", checks if either provided expression is truthy.
+
+The bang operator, !, switches the truthiness and falsiness of a value.
+
+The ternary operator is shorthand to simplify concise if...else statements.
+
+A switch statement can be used to simplify the process of writing multiple else if statements. The break keyword stops the remaining cases from being checked and executed in a switch statement.
 
 
 
