29d31f7e1e4ff446f4d512eb875a16b35d8fe16a
James Bennetts <jamesbennetts@gmail.com>
2018-10-24 18:07:26 +0100

    Scoping and Arrays
    Completed 'Training Days.'
    
    Started 'Arrays' section.

---
+++ 5. Arrays/arrays.js
@@ -0,0 +1,29 @@
+// ARRAYS
+
+New Year's Resolutions:
+1. Keep a journal 
+2. Take a falconry class
+3. Learn to juggle
+
+// In JS, looks like this:
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+
+
+// Array Literals []
+
+const hobbies = ['Drumming', 'The gym', 'Cycling'];
+
+console.log(hobbies);
+
+
+
+// Accessing Elements within Arrays. Arrays in JavaScript are 'zero-indexed'.
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+const listItem = famousSayings[0];
+
+console.log(famousSayings[0]); // 'Fortune...''
+console.log(famousSayings[2]); // 'Where there...'
+console.log(famousSayings[3]); // 'Undefined'
